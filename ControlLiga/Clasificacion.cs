3a896515cc4b9530895bcb013cdd3ec473b4b693
campanadas a la muerte <hayoto69@gmail.com>
2019-02-01 14:47:55 +0100

    CAMBIO

---
+++ ControlLiga/Clasificacion.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+using ControlLiga.Datos;
+
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ControlLiga
+{
+    public sealed class Clasificacion : Control
+    {
+        Canvas CanvasClasificacion;
+        String rectEquipo;
+        
+
+
+
+        public Clasificacion()
+        {
+            this.DefaultStyleKey = typeof(Clasificacion);
+
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                var items = new ObservableCollection<Equipo>();
+                items.Add(new Equipo { nombreEquipo = "CD ALAVES", puntos = 15, puesto = 1, escudo = new BitmapImage(new Uri("ms-appx:///Assets/ALAVES-150x150.jpg")) });               
+                ItemsSource = items;
+            }
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+           DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<Clasificacion>), typeof(Clasificacion), new PropertyMetadata(null, PintarEquipos));
+
+        private static void PintarEquipos(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PintarEquipos();
+        }
+
+        private static void PintarEquipos()
+        {
+            //throw new NotImplementedException();
+            // this.onAppyTemplate();
+        }
+
+        public ObservableCollection<Equipo> ItemsSource
+        {
+            get { return (ObservableCollection<Equipo>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+
+
+        private void AnimarGraficas(Canvas CanvasClasificacion, Rectangle grafica, Button equipo, Equipo eq )
+        {
+
+           
+        }
+
+     
+
+        /*  private void Animation_Begin(object sender, RoutedEventArgs e)
+             {
+                 myStoryboard.Begin();
+             } */
+
+        protected override void OnApplyTemplate()
+        {
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/campocanvas.jpg"));
+            
+            base.OnApplyTemplate();
+            Canvas CanvasClasificacion = (Canvas)GetTemplateChild("CanvasClasificacion");
+            CanvasClasificacion.Background = ib;
+
+
+
+            CanvasClasificacion.Children.Clear();
+          
+
+            int dib = 10;
+            int dib1 = 10;
+            foreach (Equipo item in ItemsSource)
+            {
+                ImageBrush Brocha = new ImageBrush();
+                SolidColorBrush ChampionBrush = new SolidColorBrush(Colors.Blue);
+                SolidColorBrush UefaBrush = new SolidColorBrush(Colors.Green);
+                SolidColorBrush DescensoBrush = new SolidColorBrush(Colors.Red);
+                SolidColorBrush NormalBrush = new SolidColorBrush(Colors.Yellow);
+                Button equipo = new Button();
+                equipo.Name = item.nombreEquipo;
+                equipo.Height = 50;
+                equipo.Width = 50;
+                Rectangle grafica = new Rectangle();
+                grafica.Name = item.nombreEquipo;
+                grafica.Name = item.nombreEquipo;
+                grafica.Height = item.puntos * 10;
+                grafica.Width = 50;              
+                Brocha.ImageSource = item.escudo;
+                equipo.Background = Brocha;
+
+                if (item.puesto <= 4)
+                {
+                    grafica.Fill = ChampionBrush;
+                }
+                if (item.puesto > 4 && item.puesto <= 7)
+                {
+                    grafica.Fill = UefaBrush;
+                }
+                if (item.puesto > 7 && item.puesto <= 17)
+                {
+                    grafica.Fill = NormalBrush;
+                }
+                if (item.puesto > 17)
+                {
+                    grafica.Fill = DescensoBrush;
+                }
+
+               
+
+
+                
+
+
+                CanvasClasificacion.Children.Add(equipo);
+                Canvas.SetLeft(equipo, (dib));
+                Canvas.SetTop(equipo, (800));
+               // Canvas.SetZIndex(equipo, (dib));
+               
+                CanvasClasificacion.Children.Add(grafica);
+                Canvas.SetLeft(grafica, (dib1));
+                Canvas.SetZIndex(grafica, (1000));
+                dib += 60;
+                dib1 += 60;
+               
+
+            }
+
+        }
+                }               
+            }
+      
+
+
+      
+
---
+++ ControlLiga/Datos/Equipo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ControlLiga.Datos
+{
+    public class Equipo
+    {
+        public string nombreEquipo { get; set; }
+        public int puntos { get; set; }  
+        public int puesto { get; set; }
+        public BitmapImage escudo { get; set; }
+
+    }
+
+    public class GestorEquipos
+    {
+        public static ObservableCollection<Equipo> getEquipos()
+        {
+            var Equipos = new ObservableCollection<Equipo>();
+
+            Equipos.Add(new Equipo { nombreEquipo = "CD ALAVES", puntos = 32, puesto = 5, escudo = new BitmapImage(new Uri("ms-appx:///Assets/ALAVES-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "ATH BILBAO", puntos = 26, puesto = 11, escudo = new BitmapImage(new Uri("ms-appx:///Assets/ATHLETIC-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "AT MADRID", puntos = 44, puesto = 2, escudo = new BitmapImage(new Uri("ms-appx:///Assets/ATLETICO-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "FC BARCELONA", puntos = 49, puesto = 1, escudo = new BitmapImage(new Uri("ms-appx:///Assets/BARCELONA-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "CELTA", puntos = 21, puesto = 18, escudo = new BitmapImage(new Uri("ms-appx:///Assets/CELTA-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "EIBAR", puntos = 26, puesto = 10, escudo = new BitmapImage(new Uri("ms-appx:///Assets/EIBAR-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "SPANYOL", puntos = 24, puesto = 15, escudo = new BitmapImage(new Uri("ms-appx:///Assets/ESPANYOL-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "GETAFE", puntos = 31, puesto = 6, escudo = new BitmapImage(new Uri("ms-appx:///Assets/GETAFE-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "GIRONA", puntos = 24, puesto = 14, escudo = new BitmapImage(new Uri("ms-appx:///Assets/GIRONA-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "HUESCA", puntos = 12, puesto = 20, escudo = new BitmapImage(new Uri("ms-appx:///Assets/HUESCA-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "LEGANES", puntos = 23, puesto = 16, escudo = new BitmapImage(new Uri("ms-appx:///Assets/LEGANES-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "LEVANTE", puntos = 26, puesto = 12, escudo = new BitmapImage(new Uri("ms-appx:///Assets/LEVANTE-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "RAYO", puntos = 23, puesto = 17, escudo = new BitmapImage(new Uri("ms-appx:///Assets/RAYO-VALLECANO-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "BETIS", puntos = 29, puesto = 8, escudo = new BitmapImage(new Uri("ms-appx:///Assets/REAL-BETIS-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "MADRID", puntos = 39, puesto = 3, escudo = new BitmapImage(new Uri("ms-appx:///Assets/REAL-MADRID-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "REAL SOCIEDAD", puntos = 27, puesto = 9, escudo = new BitmapImage(new Uri("ms-appx:///Assets/REAL-SOCIEDAD-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "VALLADOLID", puntos = 25, puesto = 13, escudo = new BitmapImage(new Uri("ms-appx:///Assets/REAL-VALLADOLID-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "SEVILLA", puntos = 36, puesto = 4, escudo = new BitmapImage(new Uri("ms-appx:///Assets/SEVILLA-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "VALENCIA", puntos = 29, puesto = 7, escudo = new BitmapImage(new Uri("ms-appx:///Assets/VALENCIA-150x150.jpg")) });
+            Equipos.Add(new Equipo { nombreEquipo = "VILLAREAL", puntos = 18, puesto = 19, escudo = new BitmapImage(new Uri("ms-appx:///Assets/VILLARREAL-150x150.jpg")) });
+            return Equipos;
+        }
+    }
+}
